3f43a924698883a2231ea2a2370f3299a77a56d3
Drich011 <128674882+Drich011@users.noreply.github.com>
2023-05-10 09:32:25 +0800

    checkWilds

--- src/core/settings/Functions.ts
+++ src/core/settings/Functions.ts
@@ -30,7 +30,8 @@ const hasConsecutiveSameValues=(arr:Array<any>)=> {
         arrRes.push({block:arr[i].blockNo,payout:arr[i].pattern.payout,type:arr[i].pattern.type,symbol:arr[i].pattern.symbol})
     }
      for (let i = 1; i < arr.length; i++) {
-        if ((arr[i].pattern.type === arr[i-1].pattern.type)) {
+        console.log(arr[i-1].pattern.type, "Test")
+        if (arr[i].pattern.type === arr[i-1].pattern.type || arr[i].pattern.type === 9) {
             count++;
         } else {
             break; 
