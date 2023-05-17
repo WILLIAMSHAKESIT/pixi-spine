c623cc0e69d37af3e667008700b67f8bd002969d
Drich011 <128674882+Drich011@users.noreply.github.com>
2023-05-17 09:14:08 +0800

    z

--- src/core/components/Loader.ts
+++ src/core/components/Loader.ts
@@ -61,7 +61,7 @@ export default class Loader{
         this.soundSetup(`${this.soundsPath}sfx/ui/click.mp3`,false); //1
         this.soundSetup(`${this.soundsPath}sfx/ui/hover.mp3`,false); //2
         this.soundSetup(`${this.soundsPath}sfx/reel/reel_spin.mp3`,false); //3
-        this.soundSetup(`${this.soundsPath}sfx/reel/reel_stop.mp3`,false); //4
+        this.soundSetup(`${this.soundsPath}sfx/reel/reel_stop2.mp3`,false); //4
         this.soundSetup(`${this.soundsPath}sfx/reel/common_effect.mp3`,false); //5
         
 
