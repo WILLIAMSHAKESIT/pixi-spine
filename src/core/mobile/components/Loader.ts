58649bdb2359393f40aa3f7c616f899b4ab5c5ed
Drich011 <128674882+Drich011@users.noreply.github.com>
2023-05-31 09:38:03 +0800

    fixes loading screen

--- src/core/mobile/components/Loader.ts
+++ src/core/mobile/components/Loader.ts
@@ -263,21 +263,13 @@ export default class Loader{
             this.loadingContainer.scale.set(0.9)
             this.loadingContainer.x = (this.screenSetting.baseWidth - this.loadingContainer.width)/2
             this.loadingContainer.y = (this.screenSetting.baseHeight  - this.loadingContainer.height)/2
-           
-
             this.loadingTextNew.x = (this.loadingContainer.width - this.loadingTextNew.width)/2 + 50
-
             this.soundBtnsCont.x = (this.loadingContainer.width - this.soundBtnsCont.width)/2 + 50
         }else{
             this.loadingBg.height = this.screenSetting.baseHeight
-            this.loadingContainer.scale.set(1)
-           
+            this.loadingContainer.scale.set(1)       
             this.loadingContainer.x = (this.screenSetting.baseWidth - this.loadingContainer.width)/2
             this.loadingContainer.y = (this.screenSetting.baseHeight  - this.loadingContainer.height)/2
-           
-           
-
-
             this.loadingTextNew.x = (this.loadingContainer.width - this.loadingTextNew.width)/2 
             this.soundBtnsCont.x = (this.loadingContainer.width - this.soundBtnsCont.width)/2 + 50
         }
