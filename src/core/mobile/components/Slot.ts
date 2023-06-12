d9e60a07a2465b5818cb45a9e8b4a816648767a1
Drich011 <128674882+Drich011@users.noreply.github.com>
2023-06-12 09:36:10 +0800

    Fixes in levelbar

--- src/core/mobile/components/Slot.ts
+++ src/core/mobile/components/Slot.ts
@@ -202,34 +202,44 @@ export default class Slot{
     private createLevelBar(){
         //create level bar background
         this.levelBarBg = Functions.loadTexture(this.textureArray,'main','bar_bg')
+        this.levelBarBg.width = 742
+        this.levelBarBg.height = 66
         // this.levelBarBg.x = (this.frameBorder.width - this.levelBarBg.width)+50
         this.levelBarBg.y = this.frameBorder.y * 0.7
         this.levelBarContainer.addChild(this.levelBarBg)
         //create indicator
         this.levelBarIndicator = Functions.loadTexture(this.textureArray,'main','bar_energy')
+        this.levelBarIndicator.width = 742
+        this.levelBarIndicator.height = 68
         this.levelBarIndicator.width = 0
         this.levelBarIndicator.x = this.levelBarBg.x + 5
         this.levelBarIndicator.y = this.levelBarBg.y
         this.levelBarContainer.addChild(this.levelBarIndicator)
         //create mini item
         this.itemMini = Functions.loadTexture(this.textureArray,'main','mini')
+        this.itemMini.width = 245
+        this.itemMini.height = 106
         this.itemMini.x = this.levelBarBg.x
         this.itemMini.y = this.levelBarBg.y - 30
         this.levelBarContainer.addChild(this.itemMini)
         //create major item
         this.itemMajor = Functions.loadTexture(this.textureArray,'main','major')
+        this.itemMajor.width = 266
+        this.itemMajor.height = 119
         this.itemMajor.x = this.itemMini.x + this.itemMini.width
         this.itemMajor.y = this.itemMini.y - 13
         this.levelBarContainer.addChild(this.itemMajor)
         //create grand item
         this.itemGrand = Functions.loadTexture(this.textureArray,'main','grand')
+        this.itemGrand.width = 244
+        this.itemGrand.height = 105
         this.itemGrand.x = this.itemMajor.x + this.itemMajor.width
         this.itemGrand.y = 10
         this.levelBarContainer.addChild(this.itemGrand)
 
         const style = new PIXI.TextStyle({  
             fontFamily: 'Arial',
-            fontSize: 36,
+            fontSize: 20,
             fontWeight: 'bold',
             fill: ['#ffffff', '#ffffff'], // gradient
             stroke: '#4a1850',
@@ -245,18 +255,18 @@ export default class Slot{
         });
         //create mini text
         const miniPrize = new PIXI.Text(`${json.jackpots.mini}`, style);
-        miniPrize.x = (this.itemMini.width - miniPrize.width)/2;
-        miniPrize.y = 40;
+        miniPrize.x = (this.itemMini.width - miniPrize.width)/2 - 55;
+        miniPrize.y = 15;
         this.itemMini.addChild(miniPrize)
         //create major text
         const majorPrize = new PIXI.Text(`${json.jackpots.major}`, style);
-        majorPrize.x = (this.itemMajor.width - majorPrize.width)/2;
-        majorPrize.y = 53;
+        majorPrize.x = (this.itemMajor.width - majorPrize.width)/2  - 55;
+        majorPrize.y = 21;
         this.itemMajor.addChild(majorPrize)
         //create grand text
         const grandPrize = new PIXI.Text(`${json.jackpots.grand}`, style);
-        grandPrize.x = (this.itemGrand.width - grandPrize.width)/2;
-        grandPrize.y = 40;
+        grandPrize.x = (this.itemGrand.width - grandPrize.width)/2  - 55;
+        grandPrize.y = 14;
         this.itemGrand.addChild(grandPrize)
         this.container.addChild(this.levelBarContainer)
     }
